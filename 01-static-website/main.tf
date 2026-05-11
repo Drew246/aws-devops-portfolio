5e8444086c1e2a776465fa5a18320f8c8c31a895
Andrew McCollin <andrewmccollin@Andrews-MacBook-Air.local>
2026-05-11 11:34:35 -0400

    Add CloudFront distribution to S3 static website

--- 01-static-website/main.tf
+++ 01-static-website/main.tf
@@ -68,3 +68,48 @@ resource "aws_s3_bucket_policy" "website" {
 output "website_url" {
   value = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
 }
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.website.website_endpoint
+    origin_id   = "S3-website"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-website"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.website.domain_name}"
+}
